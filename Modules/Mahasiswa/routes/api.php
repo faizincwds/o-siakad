6a15b09e533346e15594e3b12754ff7114d6d1d7
faizinwds <faizincwds@gmail.com>
2026-05-19 18:07:03 +0700

    ubah

--- Modules/Mahasiswa/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Mahasiswa\App\Http\Controllers\MahasiswaController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('mahasiswas', MahasiswaController::class)->names('mahasiswa');
-});
--- Modules/Mahasiswa/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Mahasiswa\App\Http\Controllers\MahasiswaController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('mahasiswas', MahasiswaController::class)->names('mahasiswa');
-});
