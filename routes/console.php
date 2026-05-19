5f2b029c56e4a5fdc8bd2b71806a0d1f4c0807b1
faizincwds <faizincwds@gmail.com>
2026-05-19 15:15:30 +0700

    rm

--- routes/console.php
+++ routes/console.php
@@ -1,25 +1,26 @@
 <?php
 
+// use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
+// use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('backup:run')
-    ->dailyAt('01:00')
-    ->monitorName('Daily Backup');
+// Schedule::command('backup:run')
+//     ->dailyAt('01:00')
+//     ->monitorName('Daily_Backup');
 
-Schedule::command('backup:clean')
-    ->dailyAt('02:00')
-    ->monitorName('Backup Cleanup');
+// Schedule::command('backup:clean')
+//     ->dailyAt('02:00')
+//     ->monitorName('Backup_Cleanup');
 
-Schedule::command('generate:sitemap')
-    ->daily()
-    ->monitorName('Generate Sitemap');
+// Schedule::command('generate:sitemap')
+//     ->daily()
+//     ->monitorName('Generate_Sitemap');
 
-Schedule::command('queue:restart')
-    ->daily()
-    ->monitorName('Queue Restart');
+// Schedule::command('queue:restart')
+//     ->daily()
+//     ->monitorName('Queue_Restart');
