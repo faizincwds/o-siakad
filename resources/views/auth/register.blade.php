16d5030d7b5313cc37f10a84496aa275176220f2
faizincwds <faizincwds@gmail.com>
2026-05-24 13:57:15 +0700

    e

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -93,47 +93,63 @@
                 >
 
                 <span
-                    @click="showPassword = !showPassword"
-                    class="absolute right-4 top-3.5 cursor-pointer text-gray-400"
-                >
-                    <svg
-                        x-show="!showPassword"
-                        x-transition.opacity
-                        class="h-5 w-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                        />
-                    </svg>
+    @click="showPassword = !showPassword"
+    class="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-gray-400"
+>
+    <!-- Eye Open -->
+    <svg
+        x-show="!showPassword"
+        x-transition.opacity.duration.200ms
+        x-cloak
+        class="h-5 w-5"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+        />
+    </svg>
 
-                    <svg
-                        x-show="showPassword"
-                        x-transition.opacity
-                        class="h-5 w-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.042-3.368M6.223 6.223A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.97 9.97 0 01-4.132 5.411M15 12a3 3 0 00-3-3m0 0a2.996 2.996 0 00-2.12.879M12 9l-8 8m8-8l8 8"
-                        />
-                    </svg>
-                </span>
+    <!-- Eye Closed -->
+    <svg
+        x-show="showPassword"
+        x-transition.opacity.duration.200ms
+        x-cloak
+        class="h-5 w-5"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 3l18 18"
+        />
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10.584 10.587a2 2 0 002.829 2.829"
+        />
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9.88 5.09A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.97 9.97 0 01-1.563 2.664M6.228 6.228A9.956 9.956 0 002.458 12c1.274 4.057 5.064 7 9.542 7 1.61 0 3.13-.38 4.477-1.056"
+        />
+    </svg>
+</span>
             </div>
 
             @error('password')
